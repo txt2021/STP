b53a56d452a2d7c0dff23bbb58a3abae649e0880
txt2021 <47900977+txt2021@users.noreply.github.com>
2021-06-17 14:30:14 +0300

    Add files via upload

---
+++ main.js
@@ -0,0 +1,71 @@
+
+$(function () {
+	$(window).scroll(function() {
+	    $('#learn .section-title').each(function(){
+	        var imagePos = $(this).offset().top;
+
+	        var topOfWindow = $(window).scrollTop();
+	        if (imagePos < topOfWindow+650) {
+	            $(this).addClass("fadeInLeft");
+	        }
+	    });
+	});
+	$(window).scroll(function() {
+	    $('.skill').each(function(){
+	        var imagePos = $(this).offset().top;
+
+	        var topOfWindow = $(window).scrollTop();
+	        if (imagePos < topOfWindow+650) {
+	            $(this).addClass("fadeInRight");
+	        }
+	    });
+	});
+	$(window).scroll(function() {
+	    $('.skill-travel').each(function(){
+	        var imagePos = $(this).offset().top;
+
+	        var topOfWindow = $(window).scrollTop();
+	        if (imagePos < topOfWindow+650) {
+	            $(this).addClass("fadeInRight");
+	        }
+	    });
+	});
+
+	$(window).scroll(function() {
+	    $('.skill-value').each(function(){
+	        var imagePos = $(this).offset().top;
+
+	        var topOfWindow = $(window).scrollTop();
+	        if (imagePos < topOfWindow+650) {
+	            $(this).addClass("fadeInRight");
+	        }
+	    });
+	});
+
+
+	$(window).scroll(function() {
+	    $('#mail .section-title').each(function(){
+	        var imagePos = $(this).offset().top;
+
+	        var topOfWindow = $(window).scrollTop();
+	        if (imagePos < topOfWindow+650) {
+	            $(this).addClass("fadeInRight");
+	        }
+	    });
+	});
+
+	$(window).scroll(function() {
+	    $('.form').each(function(){
+	        var imagePos = $(this).offset().top;
+
+	        var topOfWindow = $(window).scrollTop();
+	        if (imagePos < topOfWindow+650) {
+	            $(this).addClass("fadeInLeft");
+	        }
+	    });
+	});
+
+    
+	
+})
+
